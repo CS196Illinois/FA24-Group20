87458be031ee208d8abf4ba5b080dfa272ea2aee
William Lei <lishenglei2@gmail.com>
2024-10-23 23:34:32 -0500

    added functional navbar to website

--- Frontend/website/src/App.js
+++ Frontend/website/src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
+import './App.css'
+import { Route,Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import About from './components/pages/About';
+import Compare from './components/pages/Compare';
+import Home from './components/pages/Home';
+
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <>
+        <Navbar />
+        <div>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/Compare" element={<Compare />} />
+                <Route path="/About" element={<About />} />
+            </Routes>
+        </div>
+    </>
   );
 }
 
---
+++ Frontend/website/src/components/pages/Compare.js
@@ -0,0 +1,12 @@
+import './pages.css';
+import React from 'react';
+
+function Compare() {
+    return (
+        <div>
+            <h1>Compare</h1>
+        </div>
+    );
+}
+
+export default Compare;
