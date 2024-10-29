02cefe8436323239b839b3f9231fcb69ba79337e
Eshita Kulai <eshitakulai@gmail.com>
2024-10-29 15:37:01 -0500

    Completed Home and About Page template

--- Frontend/website/src/components/pages/Home.js
+++ Frontend/website/src/components/pages/Home.js
@@ -4,14 +4,17 @@ import classes from './HomeAboutPages.module.css'
 function Home() {
     return (
         <div>
+            <img src='biasbuster.png' alt="Bias Buster Logo" className={classes.center}></img>
             <h1 className={classes.titleStyle}>Bias Buster</h1>
             <h2 className={classes.comingSoonRegular}>Debunking Politics One Article at a Time</h2>
-            <a href="http://localhost:3000/Compare">
-                <button className={`${classes.button_1} ${classes.left_button}`}>Let's Compare</button>
-            </a>
-            <a href="http://localhost:3000/About">
-                <button className={`${classes.button_1} ${classes.right_button}`}>About Us</button>
-            </a>
+            <div className={classes.buttonBox}>
+                <a href="http://localhost:3000/Compare">
+                    <button className={`${classes.button_1} ${classes.left_button}`}>Compare </button>
+                </a>
+                <a href="http://localhost:3000/About">
+                    <button className={`${classes.button_1} ${classes.right_button}`}>About Us</button>
+                </a>
+            </div>
         </div>
     );
 }
