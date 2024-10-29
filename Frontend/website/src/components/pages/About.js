45895914007c9d0193fd23187aa7f8520a355038
Eshita Kulai <eshitakulai@gmail.com>
2024-10-28 21:08:52 -0500

    hi

--- Frontend/website/src/components/pages/About.js
+++ Frontend/website/src/components/pages/About.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import classes from './HomeAboutPages.module.css'
+
 
 function About() {
     return (
         <div>
-            <h1>About</h1>
+            <h1 className={classes.aboutTitleStyle}>About Us</h1>
+            <p className={classes.paragraphContent}>
+                Our app lets you compare two articles and<br></br>
+                highlight the differences between<br></br>
+                them; this is denoted by a percentage<br></br>   
+            </p>
+            <h1 className={classes.ourMissionStyle}>Our Mission</h1>
+            <p className={classes.textContent2}>
+                News Companies tell the same story using<br></br>
+                vastly different perspectives. Our app enables<br></br> 
+                you to tell these differences apart from each<br></br> 
+                other, AND highlight their bias. Bias Buster<br></br> 
+                enables YOU to determine the real story.<br></br>
+            </p>
         </div>
     );
 }
--- Frontend/website/src/components/pages/Home.js
+++ Frontend/website/src/components/pages/Home.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import classes from './HomeAboutPages.module.css'
 
 function Home() {
     return (
         <div>
-            <h1>Home</h1>
+            <h1 className={classes.titleStyle}>Bias Buster</h1>
+            <h2 className={classes.comingSoonRegular}>Debunking Politics One Article at a Time</h2>
+            <a href="http://localhost:3000/Compare">
+                <button className={`${classes.button_1} ${classes.left_button}`}>Let's Compare</button>
+            </a>
+            <a href="http://localhost:3000/About">
+                <button className={`${classes.button_1} ${classes.right_button}`}>About Us</button>
+            </a>
         </div>
     );
 }
