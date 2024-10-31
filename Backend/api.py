a5f743674dc9a5717ed0826b1a743a8b8aec852b
mb8 <mlbriones8@gmail.com>
2024-10-31 12:17:20 -0500

    Added comments to API route. Could use some refining still.

--- Backend/api.py
+++ Backend/api.py
@@ -1,23 +1,21 @@
 import web_scraper
 from flask import Flask, request, jsonify
 
-#  Create an api route that
-#       takes in a url, 
-#       scrapes the web (by calling the function that you and rachel created), and 
-#       returns the final scraped content
-
 # Creates Flask application
 app = Flask(__name__)
 
 # Creates API route
 @app.route("/api/get_article/<path:url>") 
 def get_article(url):
+    # Scrapes web by calling scrapeURL function
     heading, content, error = web_scraper.scrapeURL(url)
     data = {
         "Heading": heading,
         "Content": content,
         "Error": error
     }
+
+    # Returns the final scraped content
     return jsonify(data)
 
 # Runs Flask application (server)
