2b036b29474a7efa50b6b6447a6fdcbd53c31b39
mb8 <mlbriones8@gmail.com>
2024-11-18 17:15:08 -0600

    Modified web_scraper function

--- Backend/web_scraper.py
+++ Backend/web_scraper.py
@@ -43,31 +43,36 @@ def scrapeURL(my_url):
 
 # Takes an HTML file and returns the heading and contents of the article
 def scrapeHTMLContent(my_html_file):
-    # Retrieves the entire contents of the HTML file
-    with open(my_html_file, 'r', encoding='utf-8') as file:
-        page = file.read()
+    try: 
+        # Retrieves the entire contents of the HTML file
+        with open(my_html_file, 'r', encoding='utf-8') as file:
+            page = file.read()
 
-    # Uses BeautifulSoup to parse raw HTML data and return desired data
-    soup = BeautifulSoup(page, 'html.parser')
+        # Uses BeautifulSoup to parse raw HTML data and return desired data
+        soup = BeautifulSoup(page, 'html.parser')
     
-    # Uses soup instance to find all elements for headings and page content based on tags
-    heading = soup.find('h1').get_text()
-    content = soup.find_all('p')
+        # Uses soup instance to find all elements for headings and page content based on tags
+        heading = soup.find('h1').get_text()
+        content = soup.find_all('p')
     
-    str_content_list = [] # To store the page content as a list of Strings
-    str_content = "" # To store the page content as a single String
+        str_content_list = [] # To store the page content as a list of Strings
+        str_content = "" # To store the page content as a single String
     
-    # Prints the article heading and content to the terminal
-    print("====================HEADING====================")
-    print(heading)
-    print("====================CONTENT====================")
-    for paragraph in content:
-        str_content_list.append(paragraph.get_text()) # Adds each paragraph of page content to the list
-        str_content += paragraph.get_text() + " " # Concatenates each paragraph of page content to the String variable
-    print(str_content)
+        # Prints the article heading and content to the terminal
+        print("====================HEADING====================")
+        print(heading)
+        print("====================CONTENT====================")
+        for paragraph in content:
+            str_content_list.append(paragraph.get_text()) # Adds each paragraph of page content to the list
+            str_content += paragraph.get_text() + " " # Concatenates each paragraph of page content to the String variable
+        print(str_content)
     
-    # Returns the article heading as a String and the content as a String
-    return heading, str_content, None
+        # Returns the article heading as a String and the content as a String
+        return heading, str_content, None
+    except:
+        error = 'Failed to get page contents.'
+        print("Failed to get page contents.")
+        return None, None, error
 
 # Tests: 
 # url1 = "https://apnews.com/article/trump-california-coachella-nevada-arizona-newsom-4557c2f98ffc179178fe5b6ec5bcf8aa"
