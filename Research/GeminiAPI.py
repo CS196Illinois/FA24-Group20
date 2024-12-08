1930942fb3562939c5e97f200abcfa0669715ef5
Shrest Das <shrest@icloud.com>
2024-12-07 18:16:27 -0600

    contrasting function for two articles for a given aspect

--- Research/GeminiAPI.py
+++ Research/GeminiAPI.py
@@ -1,3 +1,4 @@
+from math import tanh
 import google.generativeai as genai
 import os
 import nltk
@@ -19,129 +20,15 @@ API_KEY = os.environ["API_KEY"]
 genai.configure(api_key=API_KEY)
 model = genai.GenerativeModel("gemini-1.5-flash")
 
-
-def provocative_language(text_to_summarize):
-
-   prompt = f"Provide the 10 most strongly worded phrases or sentence in the following text and only include them as a list separated by a new line:\n\n{text_to_summarize}"
-
-   response = model.generate_content(prompt)
-   provocative_text = response._result.candidates[0].content.parts[0].text.split("\n")
-
-   return provocative_text
-
-# def negative_sentences(text, aspects):
-#     prompt = f"in the following article:\n\n{text}\n\nProvide up to 20 of the most strongly positive, negative, or neutral phrases or sentences about {aspects[i]}. Do not include any text aside from the direct phrases from the article in your answer, and separate each phrase by a new line."
-
-
-def negative_sentences(text_to_summarize, aspect):
-
-    prompt = f"""Provide the 2-10 most negatively worded phrases or sentences against {aspect} in the following text and only include them as a list separated by a new line:
-    If the article does not contain negatively worded phrases against {aspect}, then do not include them.
-    
-    \n\n{text_to_summarize}
+"""
+FUNCTIONS TO BE USED BY THE BACKEND
 """
 
-    response = model.generate_content(prompt)
-    provocative_text = response._result.candidates[0].content.parts[0].text
+def call_gemini_api_for_summary(text_to_summarize):
 
-    provocative_text = provocative_text.split("\n")
-    return_ls = []
-    for text in provocative_text :
-        return_ls.append(text.strip("\"'"))
+    print()
 
-    return return_ls
-
-def positive_sentences(text_to_summarize, aspect):
-
-    prompt = f"""Provide the 2-10 most positively worded phrases or sentences with respect to {aspect} in the following text and only include them as a list separated by a new line:
-    If the article does not contain positively worded phrases for {aspect}, then do not include them.
-    
-    \n\n{text_to_summarize}
-"""
-
-    response = model.generate_content(prompt)
-    provocative_text = response._result.candidates[0].content.parts[0].text
-
-    provocative_text = provocative_text.split("\n")
-    return_ls = []
-    for text in provocative_text :
-        return_ls.append(text.strip("\"'"))
-
-    return return_ls
-
-def get_sentences(text, aspects):
-    d = {} # aspect : sentences
-    for aspect in aspects:
-        d[aspect] = negative_sentences(text, aspect) + positive_sentences(text, aspect)
-    return d
-
-
-def get_average_sentiment_score(text, aspects):
-    aspectToSentence = get_sentences(text, aspects)
-
-    aspect_to_score = {}
-
-    for aspect in aspectToSentence:
-        # call get_sentiment_score with aspectToSentence[aspect]
-        # add it to a dictionary
-        aspect_to_score[aspect] = get_sentences_sentiment_score(aspectToSentence[aspect], True)
-
-    return aspect_to_score
-
-
-def get_aspect_to_score_map(phrases, telemetry):
-    sentences = get_sentences()
-    # call get_sentences
-    # call get_average_sentiment_score to construct aspect_to_score_map
-    pass
-
-# def get_average_sentiment_score(text, aspects):
-
-#    positive_phrases = {}
-
-#    for i in range(len(aspects)):
-#        prompt = f"in the following article:\n\n{text}\n\nProvide up to 20 of the most strongly positive, negative, or neutral phrases or sentences about {aspects[i]}. Do not include any text aside from the direct phrases from the article in your answer, and separate each phrase by a new line."
-#        response = model.generate_content(prompt)
-#        average = get_sentiment_score(response._result.candidates[0].content.parts[0].text.split("\n"), True)
-#        print(f"SCORE FOR {aspects[i]} is {average}")
-
-#    return positive_phrases
-
-def get_sentence_sentiment_score(phrase, telemetry):
-    score = analyzer.polarity_scores(phrase)
-    for i in range(10):
-        if score['compound'] == 0:
-            score = analyzer.polarity_scores(phrase)
-        else:
-            break
-    if score['compound'] != 0:
-        return score['compound']
-    if telemetry:
-        print(f"{phrase} has a score of {score}")
-    return 0
-
-def get_sentences_sentiment_score(phrases, telemetry):
-   compound_scores = []
-   for phrase in phrases:
-       compound_scores.append(get_sentence_sentiment_score(phrase, telemetry))
-  
-   average_compound = sum(compound_scores) / len(compound_scores) if compound_scores else 0
-   return average_compound
-
-def get_sentiment_score_type(phrases, type):
-   compound_scores = []
-   for phrase in phrases:
-       score = analyzer.polarity_scores(phrase)
-       compound_scores.append(score[type])
-  
-   average_compound = sum(compound_scores) / len(compound_scores) if compound_scores else 0
-   return average_compound
-  
-def ask_gemini(prompt):
-   response = model.generate_content(prompt)
-   return response._result.candidates[0].content.parts[0].text.split("\n")
-
-def summarize(text_to_summarize):
+    print("this is my text to summarize:\n "+text_to_summarize)
 
     prompt = f"Summarize the following article:\n\n{text_to_summarize}"
 
@@ -149,6 +36,17 @@ def summarize(text_to_summarize):
     summary_text = response._result.candidates[0].content.parts[0].text
 
     return summary_text
+
+def get_constrasting_score(article1, article2, aspect):
+    output1 = get_sentiments(article1, aspect)
+    output2 = get_sentiments(article2, aspect)
+
+    avg_score1 = output1["average_sentiment_score"]
+    avg_score2 = output2["average_sentiment_score"]
+
+    contrasting_score = tanh(3 * abs(avg_score1 - avg_score2))
+
+    return contrasting_score
 
 
 """
@@ -253,12 +151,11 @@ def get_sentence_sentiment_score(phrase, telemetry):
         else:
             break
     if score['compound'] != 0:
-        return score['compound']
+        # return 2 * score['compound'] / (1.0 + abs(score['compound']))
+        return tanh(3 * score['compound'])
     if telemetry:
-        print(f"{phrase} has a score of {score}")
+        print(f"{phrase} has a score of {tanh(3 * score['compound'])}")
     return 0
-
-
 
 article = """
 ====================HEADING====================
@@ -323,6 +220,24 @@ Mark Murray is a senior political editor at NBC News.
 © 2024 NBC UNIVERSAL
 """  
 
+good_bill = """
+Bill is a capable and driven professional whose contributions have left a meaningful impact on his field. Known for his thoughtful approach to problem-solving, he combines practical know-how with a willingness to adapt to new challenges. Bill’s ventures, while not without hurdles, showcase his resilience and ability to navigate complex situations effectively.
+
+His leadership is grounded in collaboration, and his dedication to mentoring and community involvement underscores his commitment to making a positive difference. While he isn’t without flaws—like anyone, he has areas to refine—his willingness to learn and grow sets him apart as someone with considerable potential.
+
+Bill’s journey is a reminder that steady progress and a focus on improvement can lead to lasting success, even in the face of challenges.
+"""
+
+bad_bill = """
+Bill is a diligent and capable individual whose career reflects a mix of accomplishments and learning experiences. His ventures highlight a strong sense of determination and an ability to overcome obstacles, though occasional missteps have provided room for growth. Bill’s methodical approach to leadership often yields solid results, even if it sometimes takes longer to achieve his goals than planned.
+
+One of his standout qualities is his commitment to personal and professional development. He values feedback and strives to incorporate lessons learned into his work. While he may not always have the boldest ideas, his consistency and dedication to his craft earn him the respect of those around him.
+
+Ultimately, Bill exemplifies the complexities of a professional journey—marked by achievements, occasional setbacks, and a constant drive to improve.
+"""
+
 if __name__ == "__main__":
-   output = get_sentiments(article, "Trump")
-   print(json.dumps(output, sort_keys=True, indent=2))
+#    output = get_sentiments(article, "Trump")
+#    print(json.dumps(output, sort_keys=True, indent=2))
+
+   print(get_constrasting_score(bad_bill, good_bill, "Bill"))
